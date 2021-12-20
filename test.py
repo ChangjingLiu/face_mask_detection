0bac92326d4312f9ea6c9ca7f153b17cee4c5291
CJ LIU <593562829@qq.com>
2021-12-20 10:17:41 +0800

    2021/12/20 10:17

--- test.py
+++ test.py
@@ -1,10 +1,65 @@
+import os
+
+import cv2
 import numpy as np
 import torch
 import torchvision
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, transforms
 from torch.utils.data import DataLoader
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 import matplotlib.patches as patches
+import xml.etree.ElementTree as ET
+
+def read_annot(file_name, xml_dir):
+    """
+    Function used to get the bounding boxes and labels from the xml file
+    Input:
+        file_name: image file name
+        xml_dir: directory of xml file
+    Return:
+        bbox : list of bounding boxes
+        labels: list of labels
+    """
+    bbox = []
+    labels = []
+
+    annot_path = os.path.join(xml_dir, file_name[:-3] + 'xml')
+    tree = ET.parse(annot_path)
+    root = tree.getroot()
+    for boxes in root.iter('object'):
+        ymin = int(boxes.find("bndbox/ymin").text)
+        xmin = int(boxes.find("bndbox/xmin").text)
+        ymax = int(boxes.find("bndbox/ymax").text)
+        xmax = int(boxes.find("bndbox/xmax").text)
+        label = boxes.find('name').text
+        bbox.append([xmin, ymin, xmax, ymax])
+        if label == 'with_mask':
+            label_idx = 2
+        else:
+            label_idx = 1
+        labels.append(label_idx)
+
+    return bbox, labels
+
+# help function for drawing bounding boxes on image
+def draw_boxes(img, boxes, labels, thickness=1):
+    """
+    Function to draw bounding boxes
+    Input:
+        img: array of img (h, w ,c)
+        boxes: list of boxes (int)
+        labels: list of labels (int)
+
+    """
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    for box, label in zip(boxes, labels):
+        box = [int(x) for x in box]
+        if label == 2:
+            color = (0, 225, 0)  # green
+        elif label == 1:
+            color = (0, 0, 225)  # red
+        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, thickness)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 def get_device():
     ''' Get device (if GPU is available, use GPU) '''
@@ -34,6 +89,26 @@ def Faster_RCNN(device):
     return model
 
 
+def single_img_predict(img, model, nm_thrs=0.3, score_thrs=0.8):
+    test_img = transforms.ToTensor()(img)
+    model.eval()
+
+    with torch.no_grad():
+        predictions = model(test_img.unsqueeze(0).to(device))
+
+    test_img = test_img.permute(1, 2, 0).numpy()
+
+    # non-max supression
+    keep_boxes = torchvision.ops.nms(predictions[0]['boxes'].cpu(), predictions[0]['scores'].cpu(), nm_thrs)
+
+    # Only display the bounding boxes which higher than the threshold
+    score_filter = predictions[0]['scores'].cpu().numpy()[keep_boxes] > score_thrs
+
+    # get the filtered result
+    test_boxes = predictions[0]['boxes'].cpu().numpy()[keep_boxes][score_filter]
+    test_labels = predictions[0]['labels'].cpu().numpy()[keep_boxes][score_filter]
+
+    return test_img, test_boxes, test_labels
 
 
 def plot_img(img, predict, annotation):
@@ -76,7 +151,7 @@ if __name__ == '__main__':
 
     PATH = "checkpoint/model.pth"
     device = get_device()
-    model = Faster_RCNN()
+    model = Faster_RCNN(device)
     model.load_state_dict(torch.load(PATH))
     model.eval()  # 当用于inference时不要忘记添加
     # train_set = np.load("checkpoint/train_set.npy")
@@ -84,16 +159,34 @@ if __name__ == '__main__':
 
     # tr_set = prep_dataloader(train_set, config['xml_path'], 'train', config['batch_size'], config['n_jobs'])
     tt_set = prep_dataloader(test_set, config['xml_path'], 'test', config['batch_size'], config['n_jobs'])
+
     with torch.no_grad():
+        a=0
         for imgs, annotations in tt_set:
             imgs = list(img.to(device) for img in imgs)
             annotations = [{k: v.to(device) for k, v in t.items()} for t in annotations]
 
             preds = model(imgs)
+            # Prediction
+            test_img, test_boxes, test_labels = single_img_predict(imgs)
+            test_output = draw_boxes(test_img, test_boxes, test_labels)
 
-            for i in range(len(imgs)):
-                plot_img(imgs[i], preds[i], annotations[i])
-                # plot_img(imgs[i], annotations[i])
-                s = i + ".png"
-                plt.savefig("../data_set/predict/" + s)
-            break
+            # Draw the bounding box of ground truth
+            bbox, labels = read_annot(file_list[idx], config['xml_path'])
+            # draw bounding boxes on the image
+            gt_output = draw_boxes(test_img, bbox, labels)
+
+            # for i in range(2):
+            # plot_img(imgs[0], preds[0], annotations[0])
+            # plot_img(imgs[i], annotations[i])
+            # Display the result
+
+            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
+            ax1.imshow(test_output)
+            ax1.set_xlabel('Prediction')
+            ax2.imshow(gt_output)
+            ax2.set_xlabel('Ground Truth')
+
+            s = "%d.png" % a
+            plt.savefig("../data_set/predict/" + s)
+            a=a+1
